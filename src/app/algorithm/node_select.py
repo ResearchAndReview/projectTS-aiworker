9c533e42bc60130c22ca625396a93d996ab4945e
EN_SA <chlwnsgud7@gmail.com>
2025-06-10 00:46:37 +0900

    코드 컨벤션 일치

--- src/app/algorithm/node_select.py
+++ src/app/algorithm/node_select.py
@@ -3,6 +3,7 @@ from src.app.db.mysql import get_available_nodes
 def calc_node_completion_time(node):
     return node['ocr_task_size'] / node['ocr_perf'] + node['trans_task_size'] / node['trans_perf']
 
+
 def select_node_for_ocr(ocr_task_size: int):
     nodes = get_available_nodes()
     if len(nodes) == 0:
@@ -10,6 +11,7 @@ def select_node_for_ocr(ocr_task_size: int):
 
     nodes.sort(key=lambda node: calc_node_completion_time(node) + ocr_task_size / node['ocr_perf'])
     return nodes[0]
+
 
 def select_node_for_trans(trans_task_size: int):
     nodes = get_available_nodes()
