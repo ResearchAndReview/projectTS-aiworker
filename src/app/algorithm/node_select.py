9b1ee1ed48f834fa69af19375fb441cb86399458
EN_SA <chlwnsgud7@gmail.com>
2025-06-10 00:41:29 +0900

    알고리즘 구현

--- src/app/algorithm/node_select.py
+++ src/app/algorithm/node_select.py
@@ -1,22 +1,20 @@
-"""
-!READ ME BEFORE ENTERING DEEP!
-
-현재 메소드의 PARAMETER가 비어 있습니다. 뭔가 필요하다 싶으시면 넣어야 합니다.
-현재 주어진 것이 전부가 아닙니다. 더 추가할 수 있으므로 꼭 이야기하시거나 직접 추가해주시면 됩니다.
-
-"""
 from src.app.db.mysql import get_available_nodes
 
+def calc_node_completion_time(node):
+    return node['ocr_task_size'] / node['ocr_perf'] + node['trans_task_size'] / node['trans_perf']
 
-def select_node_for_ocr():
+def select_node_for_ocr(ocr_task_size: int):
     nodes = get_available_nodes()
     if len(nodes) == 0:
         return None
+
+    nodes.sort(key=lambda node: calc_node_completion_time(node) + ocr_task_size / node['ocr_perf'])
     return nodes[0]
 
-
-def select_node_for_trans():
+def select_node_for_trans(trans_task_size: int):
     nodes = get_available_nodes()
     if len(nodes) == 0:
         return None
+
+    nodes.sort(key=lambda node: calc_node_completion_time(node) + trans_task_size / node['trans_perf'])
     return nodes[0]
